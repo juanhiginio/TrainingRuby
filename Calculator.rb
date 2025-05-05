4254fdaa01e9c58392354c61d9ac25afa7261046
juanhiginio <juanhiginio06@gmail.com>
2025-05-05 15:18:44 -0500

    Simple Calculator created

---
+++ Calculator.rb
@@ -0,0 +1,9 @@
+puts "Ingresa el número 1: "
+num1 = gets.chomp().to_f # Convertir a float directamente de nuevo para poder realizar operaciones matemáticas con decimales
+puts "Ingresa el número 2: "
+num2 = gets.chomp().to_f # Convertir a float directamente de nuevo para poder realizar operaciones matemáticas con decimales
+
+puts "#{num1} + #{num2} = #{num1 + num2}"
+puts "#{num1} - #{num2} = #{num1 - num2}"
+puts "#{num1} * #{num2} = #{num1 * num2}"
+puts "#{num1} / #{num2} = #{num1 / num2}"
