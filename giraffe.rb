3c897eb4188ad0cce105f97165975fc2f17f20c4
juanhiginio <juanhiginio06@gmail.com>
2025-05-05 11:36:47 -0500

    puts - How to use puts in Ruby and how we can do shapes with this

--- giraffe.rb
+++ giraffe.rb
@@ -1,3 +1,9 @@
 # This is a simple Ruby script that prints "Hello World" to the console.
-print "Hello World"
+print "Hello World 1"
+
+puts "Hello World 2"
+
+print "Hello World 3"
+
+print "Hello World 4"
 
