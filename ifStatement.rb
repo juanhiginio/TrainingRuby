d6b4eca51240eaa19a89689f76b40a3739733e51
juanhiginio <juanhiginio06@gmail.com>
2025-05-05 17:57:54 -0500

    if statements, negation and logical operators

---
+++ ifStatement.rb
@@ -0,0 +1,38 @@
+# Simple if statement
+is_male = true
+
+if is_male
+  puts "You are a man"
+end 
+
+# Simple if-else statement
+is_married = false
+
+if is_married
+  puts "You are married"
+else
+  puts "You are not married"
+end
+
+# Simple if-else statement with multiple conditions and negation with not and ! both are valid for negation
+is_tall = true
+is_single = false
+
+if is_tall and is_single
+  puts "You are tall and single"
+elsif is_tall and  !is_single
+  puts "You are tall but not single"
+elsif not is_tall and is_single
+  puts "You are not tall but single"
+else
+  puts "You are not tall and not single"
+end
+
+# Or statement
+# The or operator is used to combine two or more conditions. If any of the conditions are true, the entire expression is true.
+is_white = false
+is_human = true
+
+if is_white or is_human
+  puts "You are a human"
+end
