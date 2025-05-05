a29ff40db0dd833f32e600f59983c538d1bf7ed4
juanhiginio <juanhiginio06@gmail.com>
2025-05-05 17:12:26 -0500

    Methods with and whithout parameters and default value to parameters

---
+++ Methods.rb
@@ -0,0 +1,25 @@
+# The methods are pieces of code that are used to perform specific tasks.
+
+puts "Whats your name: "
+name = gets.chomp()
+puts "Whats your age: "
+age = gets.chomp()
+
+def sayhi(name, age)
+    puts "Hello #{name}, you are #{age} years old"
+end
+
+def saybye
+    puts "Goodbye"
+end
+
+sayhi(name, age)
+sayhi("Mike", 35) # You can call the method with different arguments to see how it works with different inputs.
+
+saybye
+
+def sayGoodbye(name="No name", age=-1) # Default values for parameters
+    puts "Goodbye #{name}, you are #{age} years old"
+end
+ 
+sayGoodbye
