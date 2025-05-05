2a0f9b8c0758794d0c965ff679e1daa81551477b
juanhiginio <juanhiginio06@gmail.com>
2025-05-05 13:03:22 -0500

    Variables, data types in Ruby and how to work with them by concatenating them to text strings

---
+++ typesVariables.rb
@@ -0,0 +1,23 @@
+# Types of Variables 
+
+# String 
+name = "Juan Higinio"
+occupation = "Programmer"
+
+# Number - Integer
+age = 18
+
+# Number - Float
+# Float is a number with decimal points
+height = 1.80
+weight = 70.5
+
+# Boolean
+is_male = true
+is_married = false
+is_single = true
+
+# nil - null or undefined value
+# nil is a special value in Ruby that represents the absence of a value or an undefined value. It is similar to null in other programming languages..
+flaws = nil
+
