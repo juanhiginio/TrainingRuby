f6cd7fc9f0c388fea0e7051ac90ade0406d0163b
juanhiginio <juanhiginio06@gmail.com>
2025-05-05 17:31:54 -0500

    Return statements of methods

---
+++ ReturnStatements.rb
@@ -0,0 +1,20 @@
+def cube(num)
+  return num ** 3
+end
+
+puts cube(3)
+
+def cube(num)
+  return num ** 3
+end
+
+def square(num)
+  return num ** 2, "#{num} squared is #{num ** 2}"
+end
+
+puts square(6)
+puts square(6)[0] # prints the first element of the array returned by the square method
+puts square(6)[1] # prints the second element of the array returned by the square method
+
+puts square(6).class # prints the class of the object returned by the square method
+
