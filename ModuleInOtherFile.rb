2177902c9f44df66eda370dfc7dfabac4e1ea712
juanhiginio <juanhiginio06@gmail.com>
2025-05-06 16:27:35 -0500

    Modules and how we can use this in different files

---
+++ ModuleInOtherFile.rb
@@ -0,0 +1,5 @@
+require_relative 'Modules.rb'
+include Tools # This will include the methods from the Tools module into the current scope, allowing you to call them directly without prefixing them with the module name.
+
+Tools.say_hi("Juan") # This will call the say_hi method from the Tools module and print "Hello John".
+Tools.say_bye("Juan") # This will call the say_bye method from the Tools module and print "Goodbye John".
---
+++ Modules.rb
@@ -0,0 +1,13 @@
+module Tools 
+  def say_hi(name)
+    puts "Hello #{name}"
+  end
+
+  def say_bye(name)
+    puts "Goodbye #{name}"
+  end
+end
+
+include Tools # This will include the methods from the Tools module into the current scope, allowing you to call them directly without prefixing them with the module name.
+Tools.say_hi("John") # This will call the say_hi method from the Tools module and print "Hello John".
+Tools.say_bye("John") # This will call the say_bye method from the Tools module and print "Goodbye John".
