a24845e4220f66ba8dce4167fbb7d43c396d44aa
juanhiginio <juanhiginio06@gmail.com>
2025-05-06 14:29:01 -0500

    Handling errors with begin - rescue

---
+++ HandlingErrors.rb
@@ -0,0 +1,18 @@
+# This code demonstrates how to handle errors in Ruby using begin-rescue blocks.
+# It also shows how to raise a custom error and handle it.
+# Useful when we know that an error will occur, for example when dividing by zero or a type error. 
+
+begin
+  numbers = Array[3, 5, 7, 9, 11]
+  numbers["dog"] 
+  num = 10/0
+
+rescue ZeroDivisionError => e
+  # Handle the error here
+  # e.message will give you the error message
+  puts "Error: Division by zero is not allowed."
+rescue TypeError => e
+  # Handle the error here
+  # e.message will give you the error message
+  puts e
+end 
