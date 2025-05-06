82d8d44ad54fab4a0c2d44573ebd658acab0b4b1
juanhiginio <juanhiginio06@gmail.com>
2025-05-06 16:09:08 -0500

    Inheritance in classes

---
+++ Inheritance.rb
@@ -0,0 +1,29 @@
+class Chef 
+  def make_chicken
+    puts "The Chef makes chicken"
+  end
+  def make_salad
+    puts "The Chef makes salad"
+  end
+  def make_special_dish
+    puts "The Chef makes special dish"
+  end
+end
+
+class ItalianChef < Chef # ItalianChef is a subclass of Chef
+  def make_special_dish # method overriding
+    puts "The Chef makes chicken parm"
+  end
+  def make_pasta
+    puts "The Chef makes pasta"
+  end
+end
+
+chef = Chef.new() # create an object of the class Chef
+chef.make_chicken # call the method make_chicken of the class Chef
+
+italian_chef = ItalianChef.new() # create an object of the class ItalianChef
+italian_chef.make_special_dish # call the method make_special_dish of the class ItalianChef
+italian_chef.make_chicken # call the method make_chicken of the class Chef (inherited from Chef)
+italian_chef.make_pasta # call the method make_pasta of the class ItalianChef
+italian_chef.make_salad # call the method make_salad of the class Chef (inherited from Chef)
