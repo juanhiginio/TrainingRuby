95ee0022d218c983328af10878fa31707399a05f
juanhiginio <juanhiginio06@gmail.com>
2025-05-06 15:49:33 -0500

    Quiz Built

---
+++ BuildingAQuiz.rb
@@ -0,0 +1,32 @@
+class Question
+  attr_accessor :prompt, :answer
+  def initialize(prompt, answer) 
+    @prompt = prompt
+    @answer = answer
+  end
+end
+
+p1 = "What color are apples? \n a) Red \n b) Purple \n c) Orange"
+p2 = "What color are bananas? \n a) Pink \n b) Red \n c) Yellow"
+p3 = "What color are pears? \n a) Yellow \n b) Green \n c) Orange"
+
+questions = Array[
+  Question.new(p1, "a"),
+  Question.new(p2, "c"),
+  Question.new(p3, "b")
+]
+
+def run_test(questions)
+  answer = nil
+  score = 0
+  for question in questions
+    puts question.prompt
+    answer = gets.chomp().downcase
+    if answer == question.answer
+      score += 1
+    end
+  end
+  puts "You got #{score.to_s} out of #{questions.length.to_s} correct!"
+end 
+
+run_test(questions)
