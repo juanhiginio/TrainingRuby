ec7c3041c2d53cdae0959da87871350f632f10cd
juanhiginio <juanhiginio06@gmail.com>
2025-05-06 10:27:48 -0500

    Exponent Method with for loop

---
+++ ExponentMethods.rb
@@ -0,0 +1,11 @@
+def pow(base_num, pow_num)
+  result = 1
+  pow_num.times do |index|
+    result *= base_num # result = result * base_num
+  end
+  return result
+end
+
+puts pow(2, 3) # prints 8
+puts pow(3, 2) # prints 9
+puts pow(10, 10) # prints 10000000000
