d13d655b24ca9a7403b89092a256cfb7f9c857d4
juanhiginio <juanhiginio06@gmail.com>
2025-05-06 09:32:09 -0500

    While Loops - sentence and application this

---
+++ whileLoops.rb
@@ -0,0 +1,10 @@
+# While Loops 
+
+# While loops are used to execute a block of code repeatedly as long as a specified condition is true.
+
+index = 1 
+
+while index <= 5 
+  puts index 
+  index+=1
+end
